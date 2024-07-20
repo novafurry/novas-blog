56c9d36db1b368eb4fa7898e32f3103a8792b01d
Nova <nova@novafurry.win>
2024-07-20 13:04:59 +1000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: "Nova's Blog",
-  subtitle: 'The blog of a silly fox',
+  subtitle: 'A blog covering programming, cybersec, tech and more',
   lang: 'en', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 50, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/pfp.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Nova',
-  bio: 'they/she • Autistic • Software Dev',
+  bio: 'they/she • Software Dev',
   links: [
     {
       name: 'YouTube',
